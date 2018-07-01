b578bc0a9c84e792742d4c512a415cd3646b2d74
Alisson <alisson.sa.nascimento@gmail.com>
2018-06-30 23:20:52 -0300

    setting master phacil/common

--- src/Controller.php
+++ src/Controller.php
@@ -6,8 +6,7 @@ use Phacil\Core\Authorization\Auth;
 class Controller extends \Phacil\Core\Architecture\Controller{
     
     public function __construct() {
-        parent::__construct();
-       
+        parent::__construct();       
     }
     
     public function __destruct() {
